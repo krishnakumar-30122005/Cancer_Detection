481fdb497ebce05573b96f141d8ebe84718e4f57
Krishna Kumar <149188795+krishnakumar-30122005@users.noreply.github.com>
2025-09-08 20:53:46 +0530

    Update train_cnn_breast.py

--- cancerDetection/training/train_cnn_breast.py
+++ cancerDetection/training/train_cnn_breast.py
@@ -1,7 +1,4 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 
 # Dataset Path
 train_dir = r"D:\cancer detection\cancerDetection\dataset\processed\breast_cancer\Testing"
